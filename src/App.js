ae07e19b35c67c8f1d5fe03ee50112a623b7b22c
Santiago Forero Arevalo <ds.foreroa@uniandes.edu.co>
2022-05-09 20:23:37 -0500

    works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Col,
+  Container,
+  Row,
+  Button,
+  Card,
+  Form,
+  Alert,
+} from "react-bootstrap";
+import { useState } from "react";
+import { Grid } from "react-loader-spinner";
+const axios = require("axios");
 
 function App() {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(-1);
+
+  const makePrediction = () => {
+    setLoading(true);
+    axios
+      .post("https://proy1p2.herokuapp.com/predict", {
+        data: [
+          {
+            study_and_condition: text,
+          },
+        ],
+      })
+      .then(function (response) {
+        console.log(response.data.Predict);
+        setResult(response.data.Predict === "[0]" ? 0 : 1);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={12}>
+          <Card className="mt-4">
+            <Card.Header>Ingresa el texto por analizar.</Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Control
+                    onChange={(e) => {
+                      setText(e.target.value);
+                    }}
+                    type="text"
+                  />
+                  <Form.Text className="text-muted"></Form.Text>
+                </Form.Group>
+              </Form>
+            </Card.Body>
+            <Card.Footer
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                height: "60px",
+              }}
+            >
+              <Button onClick={makePrediction} variant="outline-primary">
+                Analizar texto
+              </Button>
+              {loading && (
+                <Grid
+                  height="40"
+                  width="40"
+                  blue
+                  color="blue"
+                  ariaLabel="loading"
+                />
+              )}
+            </Card.Footer>
+          </Card>
+        </Col>
+        <Col sm={12}>
+          {result === 0 && (
+            <Alert variant="danger">El paciente no es elegible</Alert>
+          )}
+          {result === 1 && (
+            <Alert variant="success">El paciente es elegible</Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
